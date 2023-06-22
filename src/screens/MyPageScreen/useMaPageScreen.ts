1cbd0477c41190bc79fccc63b15f17a86b83f17e
yudai2929 <yudai201729@icloud.com>
2023-06-22 22:30:48 +0900

    feat: いいねした投稿を取得するように追加

--- src/screens/MyPageScreen/useMaPageScreen.ts
+++ src/screens/MyPageScreen/useMaPageScreen.ts
@@ -1,13 +1,18 @@
 import { useEffect } from 'react';
-import { useFetchMySnapPosts } from '../../hooks/domain/snapPost/useFetchSnapPost';
+import {
+    useFetchLikedSnapPosts,
+    useFetchMySnapPosts,
+} from '../../hooks/domain/snapPost/useFetchSnapPost';
 import { useFetchMyUser } from '../../hooks/domain/user/useFetchUser';
 
 export const useMyPageScreen = () => {
     const { data: mySnapPosts = [] } = useFetchMySnapPosts();
     const { data: myUser } = useFetchMyUser();
+    const { data: likedSnapPost } = useFetchLikedSnapPosts();
 
     return {
         mySnapPosts,
+        likedSnapPost,
         myUser,
     };
 };
